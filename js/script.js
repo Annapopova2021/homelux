60a3408a32a89f16e330b670dbd439e2045459ba
Annapopova2021 <92753196+Annapopova2021@users.noreply.github.com>
2022-04-19 15:41:36 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -35,3 +35,26 @@ if (animItems.length > 0) {
 		animOnScroll();
 	}, 300);
 }
+
+//слайдер
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		dots:true,
+		slidesToShow:3,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
+});
